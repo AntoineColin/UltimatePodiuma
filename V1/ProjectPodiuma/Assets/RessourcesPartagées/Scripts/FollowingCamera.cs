0e9003c6da187ed0a0631d9f918b0addffd507e0
Colin antoine <antoine35.colin@gmail.com>
2018-03-02 18:47:10 +0100

    Update Bloqueur.prefab, FollowingCamera.cs, and 24 more files... Une transition caméra des plus agréable

--- V1/ProjectPodiuma/Assets/RessourcesPartagées/Scripts/FollowingCamera.cs
+++ V1/ProjectPodiuma/Assets/RessourcesPartagées/Scripts/FollowingCamera.cs
@@ -8,18 +8,23 @@ public class FollowingCamera : MonoBehaviour {
 	public float XOffset = 24;
 	public float yOffset = 13.5f;
 	public delegate void passage();
-	public Vector3 tableau;
+	public int[] tabl = new int[2];
+	private Vector3 tableau;
+	public GameObject block;
+	private Vector3 posBase = new Vector3 (12, -6.75f, -2);
 
 	void Start()
 	{
-		gameObject.transform.position = new Vector3 (6.75f, 12, -2) + Vector3.Scale (tableau, new Vector3(24)) ;
-
+		tableau = new Vector3 (tableau[0], tabl[1], 0);
+		gameObject.transform.position = posBase + Vector3.Scale (tableau, new Vector3(24,13.5f,0));
 	}
 
 	public void nextTablView(int x, int y)			//déplace la caméra vers le prochain tableau
 	{
+		Debug.Log ("coucou");
+		GameObject bloc = Instantiate (block, posBase + Vector3.Scale (tableau, new Vector3(24,13.5f,0)), transform.rotation) as GameObject;
 		tableau = new Vector3 (x, y, 0);
-		gameObject.transform.position = new Vector3 (6.75f, 12, -2) + Vector3.Scale (tableau, new Vector3(24)) ;
+		gameObject.transform.position = new Vector3 (12, -6.75f, -2) + Vector3.Scale (tableau, new Vector3(24, 13.5f, 0)) ;
 	}
-
+		
 }
--- V1/ProjectPodiuma/Assets/RessourcesPartagées/Scripts/TriggerNextTabl.cs
+++ V1/ProjectPodiuma/Assets/RessourcesPartagées/Scripts/TriggerNextTabl.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class TriggerNextTabl : MonoBehaviour {
 
-	private void OnTriggerStay2D(Collider2D collision)
+	private GameObject cam;
+	private FollowingCamera scriptCam;
+	public Vector2 tabl;
+
+	void Start(){
+		cam = GameObject.Find ("Main Camera");
+		try{
+			scriptCam = cam.GetComponent<FollowingCamera> ();
+		}catch(Exception e){
+			Debug.Log ("la camera a besoin d'un script followingCamera");
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		
+		scriptCam.nextTablView ((int)tabl.x, (int)tabl.y);
+		GameObject.Destroy (gameObject);
 	}
 }
