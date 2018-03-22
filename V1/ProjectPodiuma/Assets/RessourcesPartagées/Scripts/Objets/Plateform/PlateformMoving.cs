4be8122b87eb063ed6d9eb153ac2d87090e9dd53
MagicaNexus <matthieurode@yahoo.fr>
2018-03-22 12:29:44 +0100

    Ajouts scripts player & plateforme
    Mouvement plateforme

---
+++ V1/ProjectPodiuma/Assets/RessourcesPartagées/Scripts/Objets/Plateform/PlateformMoving.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Fonction qui permet le déplacement d'une plateforme entre deux colliders
+
+// A revoir pour les tags
+
+
+public class deplacement : MonoBehaviour {
+
+	public float speed = 0.1f;
+	bool vaADroite;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		vaADroite = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(speed,0,0));
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+
+		if (collision.gameObject.tag == "changedirection1" && vaADroite){
+			speed = -0.1f;
+			vaADroite = false;
+			Debug.Log ("collision avec changedirection1");
+		}
+
+		if (collision.gameObject.tag == "changedirection2" && !vaADroite){
+			speed = 0.1f;
+			vaADroite = true;
+		}
+	}
+}
+
