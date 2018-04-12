db83b020a105c14ba10e56e8a445ebafebefa036
Colin antoine <antoine35.colin@gmail.com>
2018-04-12 15:37:26 +0200

    Menu principal V1 terminé

---
+++ V1/ProjectPodiuma/Assets/Scenes/MenuPrincipal/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MenuManager : MonoBehaviour {
+
+	public SceneAsset niv1, niv2, niv3, niv4, niv5, nivMenu;
+
+	public void chargerScene(int niv){
+		switch(niv){
+		case 1:
+			SceneManager.LoadScene (niv1.name);
+			break;
+		case 2:
+			SceneManager.LoadScene (niv2.name);
+			break;
+		case 3:
+			SceneManager.LoadScene (niv3.name);
+			break;
+		case 4:
+			SceneManager.LoadScene (niv4.name);
+			break;
+		case 5:
+			SceneManager.LoadScene (niv5.name);
+			break;
+		default:
+			SceneManager.LoadScene (nivMenu.name);
+			break;
+		}
+	}
+
+	public void Quitter(){
+		Debug.Log ("Quitter");
+		Application.Quit ();
+	}
+}
